4601fbdacdb07da15fe593b22f7bdb6e5199b125
Lorenz B. <lorenz.baechtold@web.de>
2024-10-12 14:38:55 +0200

    feat: Add menu bar
    - Add file menu
    - Add edit menu
    - Add Sstatus bar on the Bottom

--- py_text_editor.py
+++ py_text_editor.py
@@ -1,6 +1,14 @@
 # imports
+from tkinter import *
+from tkinter import filedialog
+from tkinter import font
+
 
 # root 
+root = Tk()
+root.title("Lorry's Text Editor")
+root.geometry("1024x768")
+
 
 # Set variable for open file name
 
@@ -166,27 +174,56 @@
 
 
 # Create Main Frame
-
+my_frame = Frame(root)
+my_frame.pack(pady=5)
 
 # Create our Scrollbar For the Text Box
 
 
 # Horizontal Scrollbar
-
+text_scroll = Scrollbar(my_frame)
+text_scroll.pack(side=RIGHT, fill=Y)
 
 # Create Text Box
-
+my_text = Text(
+    my_frame, 
+    width=97, 
+    height=25, 
+    font=("Consolas", 18), 
+    selectbackground="yellow", 
+    selectforeground="black", 
+    undo=True, 
+    yscrollcommand=text_scroll.set
+)
+my_text.pack()
 
 # Configure our Scrollbar
-
+text_scroll.config(command=my_text.yview)
 
 # Create Menu
-
+my_menu = Menu(root)
+root.config(menu=my_menu)
 
 # Add File Menu
-
+file_menu = Menu(my_menu, tearoff=False)
+my_menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="New")
+file_menu.add_command(label="Open")
+file_menu.add_command(label="Save")
+file_menu.add_command(label="Save As")
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
 
 # Add Edit Menu
+edit_menu = Menu(my_menu, tearoff=False)
+my_menu.add_cascade(label="Edit", menu=edit_menu)
+edit_menu.add_command(label="Cut")
+edit_menu.add_command(label="Copy")
+edit_menu.add_command(label="Paste")
+edit_menu.add_command(label="Undo")
+edit_menu.add_command(label="Redo")
+edit_menu.add_command(label="Select All")
+edit_menu.add_command(label="Clear")
 
 
 # Add Color Menu
@@ -197,6 +234,8 @@
 
 
 # Add Status Bar To Bottom Of App
+status_bar = Label(root, text='Ready        ', anchor=E)
+status_bar.pack(fill=X, side=BOTTOM, ipady=5)
 
 
 # Edit Bindings
@@ -219,3 +258,5 @@
 
 
 # Text Color
+
+root.mainloop()

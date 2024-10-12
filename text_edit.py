ebf1f662a76cccadf37f55976e72876c8abeb7e8
Lorenz Bächtold <lorenz.baechtold@web.de>
2024-10-12 08:44:43 +0000

    feat: Add pseudocode for reference

---
+++ text_edit.py
@@ -0,0 +1,221 @@
+# imports
+
+# root 
+
+# Set variable for open file name
+
+
+# Create New File Function
+
+	# Delete previous text
+
+	# Update status bars
+
+# Open Files
+
+	# Delete previous text
+
+
+	# Grab Filename
+
+	# Check to see if there is a file name
+
+		# Make filename global so we can access it later
+
+	# Update Status bars
+
+	# Open the file
+
+	# Add file to textbox
+
+	# Close the opened file
+
+
+# Save As File
+
+		# Update Status Bars
+
+		# Save the file
+
+		# Close the file
+
+
+# Save File
+
+		# Save the file
+
+		# Close the file
+
+		# Put status update or popup code
+
+# Cut Text
+
+	# Check to see if keyboard shortcut used
+
+			# Grab selected text from text box
+
+			# Delete Selected Text from text box
+
+			# Clear the clipboard then append
+
+
+# Copy Text
+
+	# check to see if we used keyboard shortcuts
+
+		# Grab selected text from text box
+
+		# Clear the clipboard then append
+
+# Paste Text
+
+	#Check to see if keyboard shortcut used
+
+
+# Bold Text
+
+	# Create our font
+
+
+	# Configure a tag
+
+
+	# Define Current tags
+
+
+	# If statement to see if tag has been set
+
+
+# Italics Text
+
+	# Create our font
+
+
+	# Configure a tag
+
+
+	# Define Current tags
+
+
+	# If statement to see if tag has been set
+
+
+# Change Selected Text Color
+
+	# Pick a color
+
+		# Create our font
+
+
+		# Configure a tag
+
+
+		# Define Current tags
+
+
+		# If statement to see if tag has been set
+
+
+# Change bg color
+
+
+# Change ALL Text Color
+
+
+# Print File Function
+
+	#printer_name = win32print.GetDefaultPrinter()
+	#status_bar.config(text=printer_name)
+	
+	# Grab Filename
+
+
+	# Check to see if we grabbed a filename
+
+		# Print the file
+
+
+# Select all Text
+
+	# Add sel tag to select all text
+
+
+# Clear All Text
+
+
+# Turn on Night Mode
+
+	# toolbar buttons
+
+	# file menu colors
+
+
+
+# Turn Off Night Mode:
+
+	# toolbar buttons
+
+	# file menu colors
+
+
+
+
+
+
+# Create a toolbar frame
+
+
+# Create Main Frame
+
+
+# Create our Scrollbar For the Text Box
+
+
+# Horizontal Scrollbar
+
+
+# Create Text Box
+
+
+# Configure our Scrollbar
+
+
+# Create Menu
+
+
+# Add File Menu
+
+
+# Add Edit Menu
+
+
+# Add Color Menu
+
+
+# Add Options Menu
+
+
+
+# Add Status Bar To Bottom Of App
+
+
+# Edit Bindings
+
+# Select Binding
+
+
+
+#fee = "John Elder"
+#my_label = Label(root, text=fee[:-1]).pack()
+
+# Create Buttons
+
+# Bold Button
+
+# Italics Button
+
+
+# Undo/Redo Buttons
+
+
+# Text Color
